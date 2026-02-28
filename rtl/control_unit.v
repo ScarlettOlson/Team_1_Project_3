cebbb9794b141bfbadc4c65c1a8a809bda9ffced
Maggie Bowden <mmbowden@wisc.edu>
2026-02-28 11:48:25 -0600

    added checks on i_opcode[0] and i_opcode[1]

--- rtl/control_unit.v
+++ rtl/control_unit.v
@@ -33,39 +33,39 @@ module cntrUnit(
 
 );
     // Gernerate Instruction Format
-    assign o_format[0] =        !i_opcode[2] & !i_opcode[3] & i_opcode[4] & i_opcode[5] & i_opcode[6];
-    assign o_format[1] =        !i_opcode[2] & i_opcode[4] & !i_opcode[5];
-    assign o_format[2] =        !i_opcode[2] & !i_opcode[3] & !i_opcode[4] & i_opcode[5];
-    assign o_format[3] =        !i_opcode[2] & !i_opcode[3] & !i_opcode[4] & i_opcode[5] & i_opcode[6];
-    assign o_format[4] =        i_opcode[2] & i_opcode[4];
-    assign o_format[5] =        i_opcode[3] &  i_opcode[6];
+    assign o_format[0] =        !i_opcode[2] & !i_opcode[3] & i_opcode[4] & i_opcode[5] & i_opcode[6] & i_opcode[0] & i_opcode[1];
+    assign o_format[1] =        !i_opcode[2] & i_opcode[4] & !i_opcode[5] & i_opcode[0] & i_opcode[1];
+    assign o_format[2] =        !i_opcode[2] & !i_opcode[3] & !i_opcode[4] & i_opcode[5] & i_opcode[0] & i_opcode[1];
+    assign o_format[3] =        !i_opcode[2] & !i_opcode[3] & !i_opcode[4] & i_opcode[5] & i_opcode[6] & i_opcode[0] & i_opcode[1];
+    assign o_format[4] =        i_opcode[2] & i_opcode[4] & i_opcode[0] & i_opcode[1];
+    assign o_format[5] =        i_opcode[3] &  i_opcode[6] & i_opcode[0] & i_opcode[1];
 
     // ALU Control Signals
-    assign o_alu_input_sel =    (!i_opcode[2] & i_opcode[4] & !i_opcode[5]) | (i_opcode[2] & !i_opcode[3] & i_opcode[6]) | (!i_opcode[6] & i_opcode[5] & !i_opcode[4]);
-    assign o_alu_op_sel[0] =    (i_funct3[0] | (i_funct3[1] & !i_funct3[2])) & i_opcode[4] & !i_opcode[2];
-    assign o_alu_op_sel[1] =    i_funct3[1] & i_opcode[4] & i_opcode[5] & !i_opcode[2];
-    assign o_alu_op_sel[2] =    i_funct3[2] & i_opcode[4] & i_opcode[5] & !i_opcode[2];
-    assign o_alu_sub_sel =      i_opcode[4] & i_opcode[5] & i_funct7[5];
-    assign o_alu_sign_sel =     i_opcode[4] & i_funct3[0];
-    assign o_alu_arith_sel =    i_opcode[4] & i_funct7[5];
+    assign o_alu_input_sel =    (!i_opcode[2] & i_opcode[4] & !i_opcode[5] & i_opcode[0] & i_opcode[1]) | (i_opcode[2] & !i_opcode[3] & i_opcode[6] & i_opcode[0] & i_opcode[1]) | (!i_opcode[6] & i_opcode[5] & !i_opcode[4] & i_opcode[0] & i_opcode[1]);
+    assign o_alu_op_sel[0] =    (i_funct3[0] | (i_funct3[1] & !i_funct3[2])) & i_opcode[4] & !i_opcode[2] & i_opcode[0] & i_opcode[1];
+    assign o_alu_op_sel[1] =    i_funct3[1] & i_opcode[4] & i_opcode[5] & !i_opcode[2] & i_opcode[0] & i_opcode[1];
+    assign o_alu_op_sel[2] =    i_funct3[2] & i_opcode[4] & i_opcode[5] & !i_opcode[2] & i_opcode[0] & i_opcode[1];
+    assign o_alu_sub_sel =      i_opcode[4] & i_opcode[5] & i_funct7[5] & i_opcode[0] & i_opcode[1];
+    assign o_alu_sign_sel =     i_opcode[4] & i_funct3[0] & i_opcode[0] & i_opcode[1];
+    assign o_alu_arith_sel =    i_opcode[4] & i_funct7[5] & i_opcode[0] & i_opcode[1];
 
     // PC Select Control
-    assign o_jump_type_sel =    i_opcode[6] & i_opcode[5] & !i_opcode[3] & i_opcode[2];
-    assign o_jump_sel =         i_opcode[6] & i_opcode[5] & i_opcode[2];
+    assign o_jump_type_sel =    i_opcode[6] & i_opcode[5] & !i_opcode[3] & i_opcode[2] & i_opcode[0] & i_opcode[1];
+    assign o_jump_sel =         i_opcode[6] & i_opcode[5] & i_opcode[2] & i_opcode[0] & i_opcode[1];
 
     // Data Memory Control
     assign o_dmem_wr_en =       o_format[2];
-    assign o_dmem_rd_en =       !i_opcode[4] & !i_opcode[5];
+    assign o_dmem_rd_en =       !i_opcode[4] & !i_opcode[5] & i_opcode[0] & i_opcode[1];
 
     // Write Back Control
-    assign o_reg_wr_sel[0] =    i_opcode[5] & !i_opcode[6];
-    assign o_reg_wr_sel[1] =    i_opcode[3] & !i_opcode[6];
-    assign o_reg_wr_sel[2] =    i_opcode[6];
+    assign o_reg_wr_sel[0] =    i_opcode[5] & !i_opcode[6] & i_opcode[0] & i_opcode[1];
+    assign o_reg_wr_sel[1] =    i_opcode[3] & !i_opcode[6] & i_opcode[0] & i_opcode[1];
+    assign o_reg_wr_sel[2] =    i_opcode[6] & i_opcode[0] & i_opcode[1];
     assign o_reg_wr_en =        o_format[1] | o_format[4] | o_format[5];
 
 
     // Determine if the instruction is a halt 
-    assign o_halt = i_opcode[6] & i_opcode[5] & i_opcode[4];
+    assign o_halt = i_opcode[6] & i_opcode[5] & i_opcode[4] & i_opcode[0] & i_opcode[1];
    
 
 endmodule
