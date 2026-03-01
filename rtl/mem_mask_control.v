58ebf2f5319ba87e329a4bbdf4bbfa28ef7c9fa7
Scarlett Olson <Scarlett.W.Olson@gmail.com>
2026-02-28 23:46:11 -0600

    Memory Debuggin

--- rtl/mem_mask_control.v
+++ rtl/mem_mask_control.v
@@ -1,16 +1,23 @@
 `default_nettype none
 
 module memCntr(
-    input wire [2:0]  i_funct3,
-    input wire [1:0]  i_pos,
+    input wire [2:0]    i_funct3,
+    input wire [1:0]    i_pos,
+    input wire          is_store,
 
     output wire [3:0]  o_dmem_mask
 );
+    wire is_byte;
+    wire is_half;
+    wire is_word;
+    assign is_byte = !i_funct3[0] & !i_funct3[1];
+    assign is_half = i_funct3[0];
+    assign is_word = i_funct3[1];
     // Generate memory Mask
-    assign o_dmem_mask[0] = !i_pos[1] & !i_pos[0];
-    assign o_dmem_mask[1] = (!i_pos[1] & !i_pos[0]) | ((i_funct3[1] ^ i_funct3[0]) & !i_pos[0]);
-    assign o_dmem_mask[2] = (i_pos[1] & !i_pos[0]) | i_funct3[1];
-    assign o_dmem_mask[3] = i_funct3[1] | ((i_funct3[1] ^ i_funct3[0]) & (i_pos[1] ^ i_pos[0])) | (i_pos[1] & i_pos[0]);
+    assign o_dmem_mask[0] = is_word | (is_half & (i_pos == 2'b00)) | (is_byte & (i_pos == 2'b00));
+    assign o_dmem_mask[1] = is_word | (is_half & (i_pos == 2'b00)) | (is_byte & (i_pos == 2'b01));
+    assign o_dmem_mask[2] = is_word | (is_half & (i_pos == 2'b10)) | (is_byte & (i_pos == 2'b10));
+    assign o_dmem_mask[3] = is_word | (is_half & (i_pos == 2'b10)) | (is_byte & (i_pos == 2'b11));
 endmodule
 
 `default_nettype wire
--- rtl/memory.v
+++ rtl/memory.v
@@ -29,44 +29,50 @@ module mem(
     wire [4:0] shift_amt;
     assign shift_amt = {i_reg_rs2_data[1:0], 3'b000};
 
-
     // Connect Input Shifter
-    wire [31:0] dmem_input_addr;
+    wire [31:0] dmem_input_data;
     shifter memInputShifter(
-        .val(i_alu_result),
+        .val(i_reg_rs2_data),
         .shamt(shift_amt),
         .shift_right(1'b1),
         .shift_arith(1'b0),
-        .shifted_val(dmem_input_addr)
+        .shifted_val(dmem_input_data)
     );
 
     // Connect Mask Generator
     wire [3:0] mask;
     memCntr memoryControl(
         .i_funct3(i_funct3),
-        .i_pos(i_reg_rs2_data[1:0]),
+        .i_pos(i_alu_result[1:0]),
+        .is_store(i_dmem_wr_en),
 
         .o_dmem_mask(mask)
     );
 
     // Connect Memory Module Pass Through
-    assign o_dmem_addr =    dmem_input_addr;
+    assign o_dmem_addr =    {i_alu_result[31:2], 2'b00};
     assign o_dmem_ren =     i_dmem_rd_en;
     assign o_dmem_wen =     i_dmem_wr_en;
-    assign o_dmem_wdata =   i_alu_result;
+    assign o_dmem_wdata =   dmem_input_data;
     assign o_dmem_mask =    mask;
     wire [31:0] dmem_data_out;
     assign dmem_data_out =  i_dmem_rdata;
 
+    
 
+    wire [31:0] dmem_shifted_data;
     shifter memoryOutputShifter(
         .val(dmem_data_out),
         .shamt(shift_amt),
         .shift_right(1'b1),
         .shift_arith(i_funct3[2]),
-        .shifted_val(o_dmem_shifted_data)
+        .shifted_val(dmem_shifted_data)
     );
-
+    assign o_dmem_shifted_data = mask[3]                 ? dmem_shifted_data :
+                                 mask[1] & !i_funct3[2]  ? {{16{dmem_shifted_data[15]}}, dmem_shifted_data[15:0]} :
+                                 mask[1] & i_funct3[2]   ? {{16{1'b0}}, dmem_shifted_data[15:0]} :
+                                 !i_funct3[2]            ? {{24{dmem_shifted_data[7]}}, dmem_shifted_data[7:0]}   :
+                                                           {{24{1'b0}}, dmem_shifted_data[7:0]};
     
 endmodule
 
